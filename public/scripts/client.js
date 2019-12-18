20a42f5b77fe602bca519ef11b61f168d972f3ba
AliceMathews <alice_mathews@hotmail.co.uk>
2019-12-18 03:13:28 +0000

    added function to loop through 'database' and append each tweet to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,14 +5,13 @@
  */
 $(document).ready(function() { 
 
- const createTweetElement = function (tweetObj) { 
-     
+  const createTweetElement = function (tweetObj) { 
     const html = `
       <article class="tweet">
         <header>
           <div class="userIcon">
             <img class="avatar" src=${tweetObj.user.avatars}>
-            <span>   ${tweetObj.user.name}</span> 
+            <span>${tweetObj.user.name}</span> 
           </div>
           <span class="username">${tweetObj.user.handle}</span>
         </header>
@@ -30,26 +29,40 @@ $(document).ready(function() {
     return html;
   }
   
-  
-  
-  
-  
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const renderTweets = function(data) { 
+    for (const tweet of data) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  renderTweets(data);
+
 });
