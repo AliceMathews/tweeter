4a7dea0b3dc21f7b8b4115db9b31e41990f1937d
AliceMathews <alice_mathews@hotmail.co.uk>
2019-12-18 23:25:36 +0000

    new tweet now shows at the top of the page. It emptys the tweet container and rerenders

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,64 +8,51 @@ $(document).ready(function() {
   //POST tweets asynchronously
   $('#postTweet').submit(function(event) { 
     event.preventDefault();
-    console.log($(this).serialize())
-    $.ajax("/tweets/", {
-      type: "POST",
-      data: $(this).serialize()
-    })
-    .then((res) => {
-      console.log('success: ', res)
-    })
-    // .catch((err) => { 
-    //   console.log('error: ', err)
-    // })
+
+    const text = $('#newTweet').val();
+    if (text === "") { 
+      window.alert("Your tweet is empty!");
+    } else if (text.length > 140) { 
+      window.alert("Your tweet is too long!");
+    } else { 
+      $.ajax("/tweets/", {
+        type: "POST",
+        data: $(this).serialize()
+      })
+      .then((res) => {
+        console.log('success: ', res)
+        // const resArr = [];
+        // resArr.push(res)
+        // renderTweets(resArr);
+        loadTweets();
+
+      })
+      // .catch((err) => { 
+      //   console.log('error: ', err)
+      // })
+
+    }
+
+
   });
 
   //Fetch tweets
   const loadTweets = function() { 
     $.ajax("/tweets")
     .then((res) => {
-      console.log(res)
+      renderTweets(res);
     })
+    // .catch((err) => { 
+    //   console.log(err);
+    // })
   }
   loadTweets();
 
   /********************************************************************************************
   */
   const createTweetElement = function (tweetObj) { 
-    let dateString = '';
-    let timeDiff = 0;
-    let unit = '';
-    
-    const date = new Date(tweetObj.created_at)
-    const tweetDate = {};
-    tweetDate.year = date.getFullYear();
-    tweetDate.month = date.getMonth()+1;
-    tweetDate.day = date.getDate();
-
-    const todaysDate = new Date();
-    const todayDate = {};
-    todayDate.year = todaysDate.getFullYear();
-    todayDate.month = todaysDate.getMonth()+1;
-    todayDate.day = todaysDate.getDate();
-    
-    if (tweetDate.year !== todayDate.year) { 
-      timeDiff = Math.abs(tweetDate.year - todayDate.year);
-      unit = 'year';
-    } else if (tweetDate.month !== todayDate.month) { 
-      timeDiff = Math.abs(tweetDate.month - tweetDate.month)
-      unit = 'month';
-    } else { 
-      timeDiff = Math.abs(tweetDate.day - todayDate.day)
-      unit = 'day';
-    }
-    
-    if (timeDiff > 1) {
-      dateString = `${timeDiff} ${unit}s ago`
-    } else { 
-      dateString = `${timeDiff} ${unit} ago`
-    }
-
+    const date = new Date(tweetObj.created_at);
+    const dateString = timeCalc(date);
 
     const html = `
       <article class="tweet">
@@ -76,7 +63,7 @@ $(document).ready(function() {
           </div>
           <span class="username">${tweetObj.user.handle}</span>
         </header>
-        <section class="tweetContent">${tweetObj.content.text}</section>
+        <p class="tweetContent">${tweetObj.content.text}</p>
         <footer>
           <span>${dateString}</span>
           <span>
@@ -91,39 +78,46 @@ $(document).ready(function() {
   }
   
   const renderTweets = function(data) { 
+    $('#tweets-container').empty();
     for (const tweet of data) {
       const $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
   };
 
-
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
-
 });
+
+const timeCalc = function(date) { 
+  let timeDiff = 0;
+  let unit = '';
+
+  const tweetDate = {};
+  tweetDate.year = date.getFullYear();
+  tweetDate.month = date.getMonth()+1;
+  tweetDate.day = date.getDate();
+
+  const todaysDate = new Date();
+  const todayDate = {};
+  todayDate.year = todaysDate.getFullYear();
+  todayDate.month = todaysDate.getMonth()+1;
+  todayDate.day = todaysDate.getDate();
+  
+  if (tweetDate.year !== todayDate.year) { 
+    timeDiff = Math.abs(tweetDate.year - todayDate.year);
+    unit = 'year';
+  } else if (tweetDate.month !== todayDate.month) { 
+    timeDiff = Math.abs(tweetDate.month - tweetDate.month)
+    unit = 'month';
+  } else { 
+    timeDiff = Math.abs(tweetDate.day - todayDate.day)
+    unit = 'day';
+  }
+  
+  if (timeDiff > 1) {
+    return `${timeDiff} ${unit}s ago`
+  } else if (timeDiff === 1) { 
+    return `${timeDiff} ${unit} ago`
+  } else {
+    return 'Today'
+  }
+}
